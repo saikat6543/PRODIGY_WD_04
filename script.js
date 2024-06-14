bd2441fdf8b37d1f1ddef9347d4b7608d05df139
saikat6543 <124084957+saikat6543@users.noreply.github.com>
2024-06-14 20:05:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// scripts.js
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your message! I will get back to you soon.');
+});
